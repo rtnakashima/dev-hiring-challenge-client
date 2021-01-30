4153c8f56eaeb835ee9445b3169900f5b24ee116
Renan Nakashima <renan.taizo@gmail.com>
2021-01-30 20:27:52 -0300

    Adds button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    width: 12em;
+    min-width: 2em
+    min-height: 1em;
+    padding: 1em;
+    background-color: mediumseagreen;
+    outline: 0;
+    border: 0;
+    border-radius: 5px;
+    font-size: 1.3em;
+    color: white;
+    &:active {
+        background-color: seagreen;
+    }
+`
+
+export default Button;
